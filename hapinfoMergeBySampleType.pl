4a0e8c2492714df8fbcc730dd00f6db11d52754c
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-06-25 12:00:25 -0700

    Update hapinfoMergeBySampleType.pl

--- hapinfoMergeBySampleType.pl
+++ hapinfoMergeBySampleType.pl
@@ -5,6 +5,8 @@ chdir getcwd;
 my $HapinfoDIR="/home/shg047/oasis/monod/hapinfo/hapinfo";
 my $config="/home/shg047/oasis/monod/saminfo.txt";
 my $OutDIR="/home/shg047/oasis/monod/hapinfo/MergeHapinfo";
+
+chdir $HapinfoDIR;
 my @file=glob("*hapInfo.txt");
 
 open F,$config;
@@ -18,11 +20,11 @@ close F;
 
 foreach my $group(sort keys %sam){
         print "$group\n";
-        system("touch $OutDIR/$group.hapinfo.merge");
+        system("touch $OutDIR/$group.hapInfo.txt");
         foreach my $label(sort @{$sam{$group}}){
                 foreach my $file(@file){
                         if($file=~/$label/){
-                                system("cat $file >> $OutDIR/$group.hapinfo.merge");
+                                system("cat $file >> $OutDIR/$group.hapInfo.txt");
                         }
                 }
         }
